0883f199302a9349c11cc336e79b8944ff742b87
Hsu-Pei-Chun <145357312+Hsu-Pei-Chun@users.noreply.github.com>
2025-02-03 15:16:43 +0800

    Revert "feat: implement avatar upload functionality to AWS and test with Postman"
    This reverts commit c320f9e0807c14604525f5912a9aae33514d4a7e.

--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,8 @@
+from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
-    path("register/", views.register, name="register"),
-    path("login/", views.login, name="login"),
-    path("logout/", views.logout, name="logout"),
-    path("upload-avatar/", views.upload_avatar, name="upload-avatar"),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
