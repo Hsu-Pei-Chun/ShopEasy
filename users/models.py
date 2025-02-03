abb3533137f176bb6e571ce4a7865afb52cc23bf
Hsu-Pei-Chun <145357312+Hsu-Pei-Chun@users.noreply.github.com>
2025-02-03 15:16:43 +0800

    Revert "feat: implement avatar upload functionality to AWS and test with Postman"
    This reverts commit c320f9e0807c14604525f5912a9aae33514d4a7e.

--- users/models.py
+++ users/models.py
@@ -1,36 +1,3 @@
-import os
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
-import mimetypes
 
-
-def user_avatar_path(instance, filename):
-    """
-    讓檔案名稱維持 `userID_原始檔名`
-    """
-    ext = filename.split(".")[-1]  # 取得副檔名 (jpg, png, etc.)
-    filename = f"{instance.user.id}_{filename}"  # 確保包含 user.id
-    return os.path.join("avatars", str(instance.user.id), filename)
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(
-        upload_to=user_avatar_path, null=True, blank=True
-    )  # 指定正確的 upload_to
-
-    def __str__(self):
-        return self.user.username
-
-
-@receiver(pre_save, sender=UserProfile)
-def update_avatar_content_type(sender, instance, **kwargs):
-    """
-    確保上傳到 S3 的圖片擁有正確的 Content-Type
-    """
-    if instance.avatar:
-        content_type, _ = mimetypes.guess_type(instance.avatar.name)
-        if content_type:
-            instance.avatar.file.content_type = content_type
+# Create your models here.
