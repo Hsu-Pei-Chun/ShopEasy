3dbf02787a4d26daebe2ebcaa9ea8e516edd8f41
Hsu-Pei-Chun <philsophysis@gmail.com>
2025-02-03 15:18:31 +0800

    fix: restore mistakenly deleted settings in views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 import mimetypes
 import boto3
 from django.conf import settings
@@ -7,6 +9,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 from .models import UserProfile
+from rest_framework.authtoken.models import Token
 
 
 @api_view(["POST"])
